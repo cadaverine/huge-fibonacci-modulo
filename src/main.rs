c874c6011284aa96f0582e0b4e3608aa08fd30d9
cadaverine <kennville@gmail.com>
2018-08-28 23:53:27 +0300

    add 'find_remain' function

--- src/main.rs
+++ src/main.rs
@@ -18,19 +18,43 @@
 // 4) после нахождения цикла Пизано - находим остаток от деления n на длину temp-массива
 // 5) остаток от деления - индекс целевого числа в temp-массиве, достаем соответсвующее значение по индексу
 
-fn huge_fib(n: u64, m: u64) -> u64 {
-    let capacity = 10000;
+// 0, 1, 2, 3, 4, 5, 6,  7,  8,  9, 10, 11,  12
 
-    let mut remains: Vec<u64> = Vec::with_capacity(capacity);
-    let mut pizanoRemains: Vec<u64> = Vec::with_capacity(capacity);
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
 
-    n + m
+// 0, 1, 1, 2, 0, 2, 2,  1,  0,  1,  1,  2,   0
+
+fn find_remain(n: u64, m: u64) -> u64 {
+    let mut remains: Vec<u64> = vec![0, 1];
+
+    let start_pizano_idx = 0;
+    let mut end_pizano_idx = 1;
+
+    let mut cycle_value = 1;
+    let mut cycle_counter = 0;
+
+    let mut done = false;
+
+    while !done {
+        let previous_value = remains[remains.len() - 2];
+        let last_value = remains[remains.len() - 1];
+
+        let remain = (previous_value + last_value) % m;
+
+        remains.push(remain);
+
+        if n == remains.len() as u64 {
+            done = true;
+        }
+    }
+
+    *remains.last().unwrap()
 }
 
 
 fn main() {
 
-    let a = huge_fib(20, 30);
+    let a = find_remain(10, 5);
 
     println!("{}", a);
 }
